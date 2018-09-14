a6502cf1fd71a246dfca7690833fc6152b7044e1
Eiji <morinokumakamesan@gmail.com>
2018-09-14 13:03:37 +0900

    adjustmentの改正

--- adjustment.m
+++ adjustment.m
@@ -11,14 +11,19 @@ MaxSum  = zeros(0,0);
 Rise = zeros(0,0);
 Fall = zeros(0,0);
 
+Task_Num=xlsread('Task_frequency.csv','A1:A26');
+
+
 %生データを順次，行列に追加
 for i = 0:25
     if i == 0
         n = 0.1;
+        m = i + 1;
     else
         n = i * 0.2;
+        m = i + 1;
     end
-    fa = fopen(['.\' num2str(n) 'Hz.txt'],'r');    % ファイルオープンの部分の引数設定は出力したcsvファイルごとで考慮すること
+    fa = fopen(['.\' num2str(Task_Num(m)) 'Hz.txt'],'r');    % ファイルオープンの部分の引数設定は出力したcsvファイルごとで考慮すること
     dataA = fscanf(fa,'%f',[2,30000]);
     dataA = dataA.';
     fclose(fa);
